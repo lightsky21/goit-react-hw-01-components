200dacc74633d7f58ce199f756e2c19cee379fab
Mariia Telehonenko <telegonenko1@ukr.net>
2022-06-08 21:43:24 +0300

    add style for Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
+import s from './Profile.module.css'
 
 function Profile({ username, tag, location, avatar = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png", stats }) {
     return (
-        <div className="profile">
+        <div className={s.profile}>
   <div className="description">
     <img
       src={avatar}

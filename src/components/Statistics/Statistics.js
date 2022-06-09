2de7a81ca96af49374e67863d9e22cecebc0d4a3
Mariia Telehonenko <telegonenko1@ukr.net>
2022-06-09 16:43:40 +0300

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,29 +4,25 @@ import PropTypes from 'prop-types';
 function Statistics({ title, stats }) {
     
     return (
-    <section class="statistics">
-            {title ? <h2 class="title">Upload stats</h2> : null}
+    <section className="statistics">
+            {title ? <h2 className="title">Upload stats</h2> : null}
 
-  <ul class="stat-list">
-    <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
-    </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
-    </li>
-  </ul>
+        <ul className="stat-list">
+
+          {stats.map(({ id, label, percentage }) => (
+            <li key={id} className="item">
+              <span className="label">{label }</span>
+              <span className="percentage">{ percentage}%</span>
+            </li>))}
+           
+        </ul>
 </section>
 )
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.object).isRequired,
 }
 
 export default Statistics;
